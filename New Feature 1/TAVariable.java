693d9ff06d10f230c49a9ed6e74bce64c5885851
Dana <dsa28@mail.aub.edu>
2017-03-11 22:12:48 +0200

    TAVariable class

--- New Feature 1/TAVariable.java
+++ New Feature 1/TAVariable.java
@@ -1,4 +1,45 @@
 
-public class TAVariable {
-
+public class TAVariable extends TAObject{
+/*
+ * A TAVariable represents a variable value
+ * It's used in expressions to represent the value to be replaced.
+ * (In particular when using quantifiers for a domain)
+ * 
+ */
+	
+	TAObject value;
+	
+	void evaluate()
+	{
+		value.evaluate();
+	}
+	
+	void list()
+	{
+		
+	}
+	
+	String type()
+	{
+		return "var";
+	}
+	
+	void set(TAObject target)
+	{
+		value = target;
+	}
+	
+	
+	//Constructors
+	TAVariable (String name)
+	{
+		this.name = name;
+	}
+	
+	TAVariable (String name, TAObject target)
+	{
+		this.name = name;
+		value = target;
+	}
+	
 }
