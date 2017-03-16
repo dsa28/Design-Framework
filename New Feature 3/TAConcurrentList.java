4486846be6be11cdc46eb3e381ddf8486bf89904
mahmoudsafar <mahmoudsafar@outlook.com>
2017-03-16 21:17:31 +0200

    Update TAConcurrentList.java

--- New Feature 3/TAConcurrentList.java
+++ New Feature 3/TAConcurrentList.java
@@ -3,7 +3,7 @@ public class TAConcurrentList extends TAStatement {
 
 	//This should be limited to statements (can be an empty statement)
 	TAStatement l;
-	//This should be limited to atomics statements
+	//This should be limited to atomic statements
 	TAStatement s1;
 	
 	TAConcurrentList(String seqName, TAStatement l, TAStatement s1)
@@ -18,6 +18,7 @@ public class TAConcurrentList extends TAStatement {
 	{
 		//Create temp TAStatement to keep a copy of l, respecting the following statement:
 		//"That is all right hand side expressions in the assignment statements in s1 and l must be evaluated before updating the corresponding target terms."
+		//TODO: THIS SHOULD BE DIFFERENT!
 		TAStatement temp = l;
 		s1.evaluate();
 		temp.evaluate();
@@ -28,7 +29,11 @@ public class TAConcurrentList extends TAStatement {
 	
 	public void list()
 	{
-		System.out.print( s1.name + " ; " + l.name);
+		System.out.print( name + " ( ");
+		s1.list();
+		System.out.print( " ; ");
+		l.list();
+		System.out.print( " ) ");
 	}
 	
 	
