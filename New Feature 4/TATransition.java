e48dd52a43d6db5e8ef5649aac150da8e33a7222
mahmoudsafar <mahmoudsafar@outlook.com>
2017-03-18 17:37:25 +0200

    Create TATransition.java

---
+++ New Feature 4/TATransition.java
@@ -0,0 +1,18 @@
+
+public class TATransition {
+
+	//Current State
+	TAState stateFrom;
+	//Next State
+	TAState stateTo;
+	
+	//Label
+	TAPort port;
+	TABool transitionGuard;
+	TAStatement action;
+	
+	//TODO: Constructor + methods
+	
+	
+	
+}
